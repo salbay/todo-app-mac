9da43aea54b65b7f43adf90f5dfe25558d20c4e8
salimbayari <salim_bayari@hotmail.com>
2023-03-06 01:36:32 +0300

    05.03.2023 01.36 implement the Edit button and real time showing

--- app1/gui.py
+++ app1/gui.py
@@ -3,23 +3,50 @@ import PySimpleGUI as sg
 
 label1 = sg.Text("Type in a to-do")
 inp_box1 = sg.InputText(tooltip="Enter todo",key="todo")
-button = sg.Button("Add")
+button_add = sg.Button("Add")
 button_exit = sg.Button("Exit")
-
+list_box = sg.Listbox(values = functions.get_todos(), key = "todos",
+                      enable_events=True,
+                      size=[45,10])
+edit_button = sg.Button("Edit")
 window = sg.Window("My To-Do App",
-                   layout=[[label1],[inp_box1,button],[button_exit]],
+                   layout=[[label1],
+                           [inp_box1,button_add],
+                           [list_box,edit_button],
+                           [button_exit]],
                    font=("Helvetica",20))
 
 while True:
     event,values = window.read()
-    print(event)
-    print(values)
+    print(1,event)
+    print(2,values)
+    print(3,values["todos"])
+    print(4,values["todo"])
+
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            inp_box1=values["todos"]
+            todo_to_edit = values["todos"][0]
+            edited_todo = values["todo"]
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = edited_todo+"\n"
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+
+        case "complete":
+            pass
+
         case "Exit":
             print("Thank you and good Bye!")
             break
