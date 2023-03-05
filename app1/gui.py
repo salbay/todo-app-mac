6a89b5705cb21eafb80c4e3eb5452b65a4156aa3
salimbayari <salim_bayari@hotmail.com>
2023-03-05 16:42:08 +0300

    05.03.2023 16.41 updating codes about interface

--- app1/gui.py
+++ app1/gui.py
@@ -1,7 +1,11 @@
 import functions
 import PySimpleGUI as sg
 
+label1 = sg.Text("Type in a to-do")
+inp_box1 = sg.InputText(tooltip="Enter todo")
+button = sg.Button("Add")
 
-print("salimmmm")
 
-print("salimmmm bayariiiiiii")
+window = sg.Window("My To-Do App",layout=[[label1],[inp_box1,button]])
+window.read()
+window.close()
